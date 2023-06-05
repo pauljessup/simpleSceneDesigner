ab3470765b29d9b8997978796b6582ec613f9624
pauljessup <paul.jessup@gmail.com>
2023-06-05 10:00:45 -0400

    Updated examples

--- main.lua
+++ main.lua
@@ -4,11 +4,22 @@ simpleScene=require("simpleScene")
 
 
 function love.load() 
+    simpleScene:setScale(3, 3)
+
     simpleScene:addSceneType({type="nightscene", vars={}})
     simpleScene:addLayerType({type="basic", vars={}})
-    simpleScene:addObjectType({type="npc", icon=love.graphics.newImage("emily.png"), image=love.graphics.newImage("emily.png")})
+    simpleScene:addObjectType({type="npc", icon=love.graphics.newImage("emily.png"), image=love.graphics.newImage("emily.png"),
+                                update=function(self, object, dt)
+                                    --neat.
+                                end,
+                                draw=function(self, object)
+                                    love.graphics.draw(self.icon, object.x, object.y)
+                                end,
+                                })
 
     simpleScene:newScene("start", "nightscene")
+    simpleScene:addLayer({image=love.graphics.newImage("map.png"), x=0, y=0, type="basic"})
+    simpleScene:addObject({type="npc", x=100, y=20})
 end
 
 function love.update(dt)
