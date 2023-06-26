b721d71ce1c650fb6bcd6c0d22012a595b5cdd02
pauljessup <paul.jessup@gmail.com>
2023-06-26 09:49:39 -0400

    Last bit of cleanup for version 1.0

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
---this will be the main example file.
+--Here is just a simple example on how to use it.
 simpleScene=require("simpleSceneDesigner")
 love.graphics.setDefaultFilter("nearest","nearest")
 
@@ -9,24 +9,6 @@ function love.load()
     --we set the editor asset directory to be editorAssets
     simpleScene:init({directories={editor="editorAssets"}})
 
-    --as you can see here, you can add draw functions to the object type that are called instead of the regular draw function.
-    --this allows for animations/etc. what's passed- self is template, object is the instanatiated object, simpleScene is the simpleScene table.
-    --other functions-
-    -- init(self, object, simpleScene)
-    -- update(self, object, simpleScene, dt)
-    -- draw(self, object, simplescene)
-    --[[
-    simpleScene:addObjectType({type="npc", image="emily.png",
-                                        draw=function(self, object, simpleScene)
-                                            love.graphics.draw(self.image, object.x, object.y)
-                                        end,
-                                        })
-
-    simpleScene:addObjectType({type="tree", image="tree.png"})
-
-    simpleScene:newScene({name="", x=0, y=0})
-    simpleScene:addObject({type="npc", x=100, y=20, layer=1})
-    ]]
     simpleScene:newScene({name="", x=0, y=0})
     simpleScene:startEditing()
 end
